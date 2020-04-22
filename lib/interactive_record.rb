6cc18615e0966ba866cb4cccea03c6c1009e76fe
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-22 20:40:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,8 +50,8 @@ class InteractiveRecord
     
     def save 
       sql = "INSERT #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-       DB.[:conn].execute(sql)
-       @id = DB
+       DB[:conn].execute(sql)
+       @id = DB[:conn].execute(sql)
     end 
     
     
