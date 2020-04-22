e5388ddd430857eab73c5416bf7958faf0e3df14
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-22 21:05:35 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -64,14 +64,13 @@ class InteractiveRecord
     def self.find_by(attribute)
       attribute_key = attribute.keys.join()
       attribute_value = attribute.values.first 
-      sql = <<-SQL "SELECT * FROM #{self.table_name} 
-      WHERE '#{attribute_key}' = '#{attribute_value}'
-      LIMIT 1" 
+      sql = <<-SQL 
+      SELECT * FROM #{self.table_name} 
+      WHERE #{attribute_key} = "#{attribute_value}"
+      LIMIT 1 
       SQL
       
       DB[:conn].execute(sql)
     end 
-    
-    
    
 end
