759f226604a53019f3cc9ebca65cb28933c965ae
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-22 20:33:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,8 +41,8 @@ class InteractiveRecord
     
     def values_for_insert
       values = []
-      self.class.column_names.each do |col| 
-        values << "(send#{col unless col == "nil"
+      self.class.column_names.each do |col_name| 
+        values << "#{send(col_name) unless col_name == "nil"
     end 
     
     def save 
